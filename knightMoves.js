02ddd3eab30650c1699db184e4a29ddcecc01001
benjaminfrias <benjaminfrias2004@gmail.com>
2024-12-20 13:17:41 -0400

    add comments

--- knightMoves.js
+++ knightMoves.js
@@ -16,7 +16,9 @@ function knightMoves(start, target) {
 	while (queue.length) {
 		let [current, steps] = queue.shift();
 
+		// Target found
 		if (current[0] === target[0] && current[1] === target[1]) {
+			// Reconstruct path
 			let path = [current];
 
 			while (current !== start) {
@@ -24,6 +26,7 @@ function knightMoves(start, target) {
 				path.unshift(current);
 			}
 
+			// Output result
 			console.log(`You made it in ${steps} moves! Here's your path:`);
 			for (const move of path) {
 				console.log(move);
@@ -31,14 +34,17 @@ function knightMoves(start, target) {
 			return;
 		}
 
+		// Visit all possible next positions from current position
 		for (const move of knightMoves) {
 			const nextX = current[0] + move[0];
 			const nextY = current[1] + move[1];
 
+			// Check chessboard constraints
 			if (nextX >= 0 && nextX < 8 && nextY >= 0 && nextY < 8) {
 				const nextPosition = [nextX, nextY];
 				const nextPositionString = nextPosition.toString();
 
+				// Add unvisited positions to the queue and the map with its predecesor
 				if (!visited.has(nextPositionString)) {
 					visited.set(nextPositionString, current);
 					queue.push([nextPosition, steps + 1]);
